0bafa41981bcdf0a77262c974d7735d761d2d692
MeVickeyKr <vk7743593@gmail.com>
2024-08-22 22:15:30 +0530

    first commit

---
+++ TODOList/Models/TodoList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TODOList.Models;
+
+public partial class TodoList
+{
+    public int Id { get; set; }
+
+    public string Task { get; set; } = null!;
+
+    public bool Iscompleted { get; set; }
+
+    public string CreatedUserId { get; set; } = null!;
+
+    public DateTime CreatedDateTime { get; set; }
+
+    public string ModifieduserId { get; set; } = null!;
+
+    public DateTime ModifiedDateTime { get; set; }
+}
